2a923f48e0dedd030284340b2cae5f1dc4fdffaa
Alex <strelchenko.aleksandr@yandex.ru>
2025-11-09 00:29:13 +0300

    Initial commit

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -3,7 +3,7 @@
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    // I started
   });
 
   test('should subtract two numbers', () => {
